eae26680728968c57a917595c2430c5512cc51ad
vermanish <nishverma11@gmail.com>
2016-01-24 19:54:25 +0530

    modified

--- calculator.py
+++ calculator.py
@@ -5,70 +5,22 @@ f=""
 def clear():
 	txtDisplay.delete(0,END)
 	return
-def dot1():
-	txtDisplay.insert(END,'.')
+
+
+def appen(x):
+	txtDisplay.insert(END,x)
 	return
 
-def appen0():
-	txtDisplay.insert(END,'0')
-	return
-def appen1():
-	txtDisplay.insert(END,'1')
-	return
-def appen2():
-	txtDisplay.insert(END,'2')
-	return
-def appen3():
-	txtDisplay.insert(END,'3')
-	return
-def appen4():
-	txtDisplay.insert(END,'4')
-	return
-def appen5():
-	txtDisplay.insert(END,'5')
-	return
-def appen6():
-	txtDisplay.insert(END,'6')
-	return
-def appen7():
-	txtDisplay.insert(END,'7')
-	return
-def appen8():
-	txtDisplay.insert(END,'8')
+def operation(x):
+	global s
+	s=txtDisplay.get()
+	global f
+	f=x
+	txtDisplay.delete(0,END)
 	return
 
-def appen9():
-	txtDisplay.insert(END,'9')
-	return
 
-def divide1():
-	global s
-	s=txtDisplay.get()
-	global f
-	f='/'
-	txtDisplay.delete(0,END)
-	return
-def multiply1():
-	global s
-	s=txtDisplay.get()
-	global f
-	f='*'
-	txtDisplay.delete(0,END)
-	return
-def minus1():
-	global s
-	s=txtDisplay.get()
-	global f
-	f='-'
-	txtDisplay.delete(0,END)
-	return
-def sum1():
-	global s
-	s=txtDisplay.get()
-	global f
-	f='+'
-	txtDisplay.delete(0,END)
-	return
+
 def dequal():
 	r=int(txtDisplay.get())
 	
@@ -102,46 +54,46 @@ topframe=Frame(root)
 topframe.pack(side=TOP)
 txtDisplay=Entry(frame,textvariable = num1, bd =20 , insertwidth =1 ,font= 30 )
 txtDisplay.pack(side=TOP)
-button1 = Button(topframe, padx=16, pady=16 ,bd=8 ,text="1",fg="black",command=appen1)
+button1 = Button(topframe, padx=16, pady=16 ,bd=8 ,text="1",fg="black",command=lambda: appen('1'))
 button1.pack(side=LEFT)
-button2 = Button(topframe, padx=16, pady=16 ,bd=8 ,text="2",fg="black",command=appen2)
+button2 = Button(topframe, padx=16, pady=16 ,bd=8 ,text="2",fg="black",command=lambda: appen('2'))
 button2.pack(side=LEFT)
-button3 = Button(topframe, padx=16, pady=16 ,bd=8 ,text="3",fg="black",command=appen3)
+button3 = Button(topframe, padx=16, pady=16 ,bd=8 ,text="3",fg="black",command=lambda: appen('3'))
 button3.pack(side=LEFT)
-button4 = Button(topframe, padx=16, pady=16 ,bd=8 ,text="4",fg="black",command=appen4)
+button4 = Button(topframe, padx=16, pady=16 ,bd=8 ,text="4",fg="black",command=lambda: appen('4'))
 button4.pack(side=LEFT)
 
 frame2=Frame(root)
 frame2.pack(side=TOP)
-button1 = Button(frame2, padx=16, pady=16 ,bd=8 ,text="5",fg="black",command=appen5)
+button1 = Button(frame2, padx=16, pady=16 ,bd=8 ,text="5",fg="black",command=lambda: appen('5'))
 button1.pack(side=LEFT)
-button2 = Button(frame2, padx=16, pady=16 ,bd=8 ,text="6",fg="black",command=appen6)
+button2 = Button(frame2, padx=16, pady=16 ,bd=8 ,text="6",fg="black",command=lambda: appen('6'))
 button2.pack(side=LEFT)
-button3 = Button(frame2, padx=16, pady=16 ,bd=8 ,text="7",fg="black",command=appen7)
+button3 = Button(frame2, padx=16, pady=16 ,bd=8 ,text="7",fg="black",command=lambda: appen('7'))
 button3.pack(side=LEFT)
-button4 = Button(frame2, padx=16, pady=16 ,bd=8 ,text="8",fg="black",command=appen8)
+button4 = Button(frame2, padx=16, pady=16 ,bd=8 ,text="8",fg="black",command=lambda: appen('8'))
 button4.pack(side=LEFT)
 
 frame3=Frame(root)
 frame3.pack(side=TOP)
-button1 = Button(frame3, padx=16, pady=16 ,bd=8 ,text="9",fg="black",command=appen9)
+button1 = Button(frame3, padx=16, pady=16 ,bd=8 ,text="9",fg="black",command=lambda: appen('9'))
 button1.pack(side=LEFT)
-button2 = Button(frame3, padx=16, pady=16 ,bd=8 ,text="0",fg="black")
+button2 = Button(frame3, padx=16, pady=16 ,bd=8 ,text="0",fg="black",command=lambda: appen('0'))
 button2.pack(side=LEFT)
-button3 = Button(frame3, padx=16, pady=16 ,bd=8 ,text=".",fg="black",comman=dot1)
+button3 = Button(frame3, padx=16, pady=16 ,bd=8 ,text=".",fg="black",command=lambda: appen('.'))
 button3.pack(side=LEFT)
-button4 = Button(frame3, padx=16, pady=16 ,bd=8 ,text="=",fg="black",command=dequal)
+button4 = Button(frame3, padx=16, pady=16 ,bd=8 ,text="=",fg="black",command= dequal)
 button4.pack(side=LEFT)
 
 frame2=Frame(root)
 frame2.pack(side=TOP)
-button1 = Button(frame2, padx=16, pady=16 ,bd=8 ,text="+",fg="black",command=sum1)
+button1 = Button(frame2, padx=16, pady=16 ,bd=8 ,text="+",fg="black",command=lambda: operation('+'))
 button1.pack(side=LEFT)
-button2 = Button(frame2, padx=16, pady=16 ,bd=8 ,text="-",fg="black",command=minus1)
+button2 = Button(frame2, padx=16, pady=16 ,bd=8 ,text="-",fg="black",command=lambda: operation('-'))
 button2.pack(side=LEFT)
-button3 = Button(frame2, padx=16, pady=16 ,bd=8 ,text="*",fg="black",command=multiply1)
+button3 = Button(frame2, padx=16, pady=16 ,bd=8 ,text="*",fg="black",command=lambda: operation('*'))
 button3.pack(side=LEFT)
-button4 = Button(frame2, padx=16, pady=16 ,bd=8 ,text="/",fg="black",command=divide1)
+button4 = Button(frame2, padx=16, pady=16 ,bd=8 ,text="/",fg="black",command=lambda: operation('/'))
 button4.pack(side=LEFT)
 
 frame5=Frame(root)
